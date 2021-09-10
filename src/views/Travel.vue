ec8437df88c81facf63f3efa4cfd8bbd58b5b322
Sterling Heibeck <sheibeck@gmail.com>
2021-09-10 08:09:47 -0400

    Add StepDetails component

--- src/views/Travel.vue
+++ src/views/Travel.vue
@@ -10,44 +10,7 @@
           </div>
         </div>      
       </div>
-
-      <div v-if="activeStep" class="col" id="activeStep">
-        <div class="card">
-          <h6>{{activeStep.stepDetails.title}}</h6>
-          <div class="card-body" v-for="(input, index) in activeStep.inputs" :key="index">
-            <div v-if="input.inputType == stepInputType.Roll">
-              <i class="fas fa-dice pe-auto d-print-none fa-2x" @click="activeStep.processInput(input, $event)"></i> {{input.text}}
-            </div>
-            <div v-if="input.inputType == stepInputType.TableResult">
-              <i class="fas fa-dice pe-auto d-print-none fa-2x" @click="activeStep.processInput(input, $event)"></i> {{input.text}}
-            </div>   
-
-            <div v-if="input.inputType == stepInputType.Input" class="input-group input-group-sm">
-              <span class="input-group-text" id="rivals-addon">{{input.text}}</span>
-              <input type="number" class="form-control" placeholder="0" :aria-label="input.text" 
-                min="0" v-model.number="input.value" />
-            </div>
-            <div v-if="input.inputType == stepInputType.YesNo" class="input-group input-group-sm">
-              <div class="input-group-text">
-                <input class="form-check-input mt-0" type="checkbox" value="" :aria-label="input.text" 
-                  v-model="input.value" />
-              </div>
-              <span class="input-group-text" id="savvy-addon">{{input.text}}</span>
-            </div>
-          </div>
-          <hr />
-          <div class="card-body" v-html="activeStep.results">
-          </div>
-          <div class="card-footer" v-html="activeStep.breakdown">
-            
-          </div>
-        </div>
-      </div>  
-      <div v-else id="activeStep">
-        <div class="card">
-          <h6>Select a step.</h6>
-        </div>
-      </div>  
+      <StepDetails :activeStep="activeStep" />
     </div>
 </div>
    
@@ -57,10 +20,12 @@
 import Vue from 'vue';
 import { CampaignStepResult, StepInputType }  from '../js/fiveParsecs';
 import { Step, SubStep } from "../js/fiveParsecsEnums";
+import StepDetails from '@/components/StepDetails.vue';
 
 export default Vue.extend({
   name: "Travel",
-  components: {    
+  components: { 
+    StepDetails,   
   },
   mounted() {   
   }, 
@@ -100,17 +65,5 @@ export default Vue.extend({
 
   .col { 
     page-break-inside: avoid;
-  }
-
-  
-  .fa-dice {
-    -moz-transition: all .5s linear;
-    -webkit-transition: all .5s linear;
-    transition: all .5s linear;
-  }
-  .fa-dice.roll {
-      -moz-transform:rotate(360deg);
-      -webkit-transform:rotate(360deg);
-      transform:rotate(360deg);
   }
 </style>
